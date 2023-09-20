0f22237d8dcc4eee9f8ffc48fe2c015beccd38e8
Adam Paul <58442068+mercuryadam@users.noreply.github.com>
2023-09-20 14:55:08 -0400

    Adding things for sent. an.

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+# Initialize the VADER sentiment intensity analyzer
+sia = SentimentIntensityAnalyzer()
+
+# Sample text
+sample_text1 = "I love this product! It's amazing."
+sample_text2 = "This is terrible, I hate it."
+
+# Get the sentiment score
+score1 = sia.polarity_scores(sample_text1)
+score2 = sia.polarity_scores(sample_text2)
+
+# Interpret the score
+def interpret_score(score):
+    compound = score['compound']
+    if compound >= 0.05:
+        return "Positive"
+    elif compound <= -0.05:
+        return "Negative"
+    else:
+        return "Neutral"
+
+print(f"Sentiment of text 1: {interpret_score(score1)}")
+print(f"Sentiment of text 2: {interpret_score(score2)}")
+
 @app.route('/ask', methods=['POST'])
 def ask_question():
     question = request.json['question']
