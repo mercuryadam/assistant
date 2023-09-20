a112d771eaf87b25356e8d1115b6504ff9bf6e33
Adam Paul <58442068+mercuryadam@users.noreply.github.com>
2023-09-20 00:07:02 -0400

    seeing if I got it working yet

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ function askQuestion() {
     const question = document.getElementById("userQuestion").value;
     // Make an API call to your Python program here
 }
+
 function getPosts() {
     FB.api('/me/posts', function (response) {
         // Do something with the posts
     });
 }
+
 function checkLoginState() {
     FB.getLoginStatus(function (response) {
         // handle the response
     });
 }
-
