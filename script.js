81065f7125a64b84a4777ce0bc15a79eb30b8f50
Adam Paul <58442068+mercuryadam@users.noreply.github.com>
2023-09-19 23:47:37 -0400

    adding FB login

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+export FLASK_APP = app.py  # On Linux / macOS
+set FLASK_APP = app.py     # On Windows
+
+
 function askQuestion() {
     const question = document.getElementById("userQuestion").value;
     // Make an API call to your Python program here
