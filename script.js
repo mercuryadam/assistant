36eb49eafe35532194a431003195023f2ce01980
Adam Paul <58442068+mercuryadam@users.noreply.github.com>
2023-09-19 23:59:17 -0400

    trying FB login again

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-
-export FLASK_APP = app.py  # On Linux / macOS
-set FLASK_APP = app.py     # On Windows
-
-
 function askQuestion() {
     const question = document.getElementById("userQuestion").value;
     // Make an API call to your Python program here
 }
+function getPosts() {
+    FB.api('/me/posts', function (response) {
+        // Do something with the posts
+    });
+}
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        // handle the response
+    });
+}
+
