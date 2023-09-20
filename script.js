6a09e755fbbcdadc8574c0a5bb3ce3ad769e9c01
Adam Paul <58442068+mercuryadam@users.noreply.github.com>
2023-09-20 00:21:21 -0400

    this stuff again

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-function askQuestion() {
-    const question = document.getElementById("userQuestion").value;
-    // Make an API call to your Python program here
+// Function to handle Facebook login
+function loginWithFacebook() {
+    FB.login(function (response) {
+        if (response.authResponse) {
+            // User is logged in and has authenticated your app
+            console.log('User is logged in, token:', response.authResponse.accessToken);
+            // Call a function to do something after login (e.g., fetch user data)
+            afterLogin();
+        } else {
+            // User canceled the login or did not fully authorize your app
+            console.log('User canceled login or did not fully authorize your app.');
+        }
+    }, { scope: 'public_profile,email' }); // Define the requested permissions here
 }
 
-function getPosts() {
-    FB.api('/me/posts', function (response) {
-        // Do something with the posts
-    });
-}
-
-function checkLoginState() {
-    FB.getLoginStatus(function (response) {
-        // handle the response
+// Function to perform actions after a successful login
+function afterLogin() {
+    // Example: Get user's basic information
+    FB.api('/me', 'GET', { fields: 'id,first_name,last_name,email' }, function (response) {
+        console.log('User information:', response);
+        // You can perform actions with the user's information here
     });
 }
